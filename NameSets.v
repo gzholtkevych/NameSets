5d6a5d7acd99d96c2f66c722ace07735a00d87c9
gzholtkevych <g.zholtkevych@gmail.com>
2023-10-22 19:22:01 +0300

    Update NameSets.v

--- NameSets.v
+++ NameSets.v
@@ -22,80 +22,77 @@ Module Type NAME.
   Parameter eq_dec : ∀ x y : name, {x = y} + {x ≠ y}.
 End NAME.
 
-Module nat_as_NAME : NAME with Definition name := nat.
-  Definition name := nat.
-  Definition id := fun x : name => x.
+Module Names (M : NAME) <: NAME with Definition name := M.name.
 
-  Lemma id_inj : ∀ x y : name, id x = id y → x = y.
-  Proof. intros. assumption. Qed.
+  Definition name := M.name.
+  Definition id := M.id.
+  Definition id_inj := M.id_inj.
+  Definition id_surj := M.id_surj.
+  Definition eq_dec := M.eq_dec.
+  
 
-  Lemma id_surj : ∀ n : nat, ∃ x : name, id x = n.
-  Proof. intro. now exists n. Qed.
+  Inductive increasing : list name → Prop :=
+    | inc0 : increasing []
+    | inc1 : ∀ n, increasing [n]
+    | incS :
+        ∀ n m lst, 
+          M.id n < M.id m → increasing (m :: lst) → increasing (n :: m :: lst).
 
-  Definition eq_dec : ∀ x y : name, {x = y} + {x ≠ y}.
+  Definition inc_dec : ∀ lst : list name, {increasing lst} + {¬ increasing lst}.
   Proof.
-    intros. destruct (Nat.eq_dec x y) as [HEq | HNeq];
-    [ now left | now right ].
+    intro.
+    destruct lst as [| n lst'].
+    - left. constructor.
+    - revert n. induction lst' as [| m lst'' IHlst'']; intro.
+      + left. constructor.
+      + destruct (lt_eq_lt_dec (M.id n) (M.id m)) as [Hle | HGt];
+        try destruct Hle as [Hlt | Heq].
+        * elim (IHlst'' m); intro H; [ left | right ];
+          try now constructor.
+          intro.
+          assert (increasing (m :: lst'')). { now inversion_clear H0. }
+          contradiction.
+        * right. intro. inversion_clear H. rewrite Heq in H0.
+          now apply Nat.lt_irrefl with (M.id m).
+        * right. intro. inversion_clear H.
+          apply Nat.lt_irrefl with (M.id n).
+          now apply Nat.lt_trans with (M.id m).
   Defined.
-End nat_as_NAME. 
 
-Module NameSet (M : NAME).
-  Import M.
+  Definition NameSet := {lst : list name | increasing lst}.
+  Coercion toList := fun ns : NameSet => proj1_sig ns.
 
-  Check name.
-End NameSet.
+  Definition In_dec : 
+    ∀ (n : name) (ns : NameSet), {In n ns} + {¬ In n ns}.
+  Proof.
+    intros.
+    destruct ns as [lst H]. simpl. clear H.
+    induction lst as [| m lst' IHlst'].
+    - right. intro. inversion_clear H.
+    - destruct (M.eq_dec n m) as [Heq | Hneq];
+      destruct (inc_dec lst') as [H1 | H1];
+      try destruct(IHlst' H1) as [H2 | H2]; clear H1;
+      try now (left; rewrite <- Heq; left).
+      + destruct IHlst' as [H | H]; [ left | right ].
+        * now right.
+        * intro. apply H. elim H0; intro; 
+          [ symmetry in H1; contradiction | assumption ].
+      + destruct IHlst' as [H | H].
+        * left. now right.
+        * right. intro. simpl in H0. elim H0; intro;
+         [ symmetry in H1; contradiction | contradiction ].
+  Defined.
 
+  Definition nothing : NameSet.
+  Proof. exists []. constructor. Defined.
 
-
-
-(* -----------------------------------------------------------------------------
-   The predicate for recognising lists of natural numbers has been sorted by
-   increasing. 
-   -------------------------------------------------------------------------- *)
-Inductive increasing : list nat → Prop :=
-  | inc0 : increasing []
-  | inc1 : ∀ n, increasing [n]
-  | incS : ∀ n m ns, 
-      n < m → increasing (m :: ns) → increasing (n :: m :: ns).
-
-Definition increasing_dec :
-(* the predicate is decidable ----------------------------------------------- *)
-  ∀ lst : list nat, {increasing lst} + {¬ increasing lst}.
-Proof.
-  intro.
-  destruct lst as [| n lst'].
-  - left. constructor.
-  - revert n. induction lst' as [| m lst'' IHlst''].
-    + left. constructor.
-    + intro. destruct (lt_eq_lt_dec n m) as [Hle | Hgt];
-      try destruct Hle as [Hlt | Heq].
-      * { elim (IHlst'' m); intro H.
-        - left. now constructor.
-        - right. intro H1. apply H. now inversion_clear H1. }
-      * right. intro H. inversion_clear H. rewrite Heq in H0.
-        now apply Nat.lt_irrefl with m.
-      * right. intro H. inversion_clear H.
-        apply Nat.lt_irrefl with m. now apply Nat.lt_trans with n.
-Defined.
-(* ========================================================================== *)
-
-(* -----------------------------------------------------------------------------
-   The type 'NameSet' is used for modelling finite subsets of names,
-   with the coercion 'toList' ensuring to operate with inhabitants of 
-   this type as with lists. 
-   -------------------------------------------------------------------------- *)
-Definition NameSet : Set := {lst : list nat | increasing lst}.
-Coercion toList := fun ns : NameSet => proj1_sig ns.
-(* -------------------------------------------------------------------------- *)
-
-Fixpoint aux_inject (n : nat) (lst : list nat) : list nat :=
-(* the auxiliary function for injecting a natural number 'n' into a list of
-   natural numbers 'lst' before the first member of the list that is greater 
-   than 'n'.
+  Fixpoint aux_inject (n : name) (lst : list name) : list name :=
+  (* the auxiliary function for injecting a name 'n' into a list of names 'lst'
+     before the first member of the list that has 'id' greater than 'id n'.
    -------------------------------------------------------------------------- *)
     match lst with
     | []        => [n]
-    | m :: lst' => match (lt_eq_lt_dec n m) with
+    | m :: lst' => match (lt_eq_lt_dec (M.id n) (M.id m)) with
         | inleft Hle => match Hle with
             | left _  => n :: m :: lst'
             | right _ => m :: lst'
@@ -104,30 +101,106 @@ Fixpoint aux_inject (n : nat) (lst : list nat) : list nat :=
         end
     end.
 
-Definition inject (n : nat) (ns : NameSet) : NameSet.
-(* the function Injects a natural number 'n' into a finite set of natural
-   numbers 'ns'.
-   -------------------------------------------------------------------------- *)
-Proof.
-  destruct ns as (lst, H). pose (aux_inject n lst) as nlst.
-  exists nlst. subst nlst.
-  destruct lst as [| m lst'].
-  - constructor.
-  - simpl. destruct (lt_eq_lt_dec n m) as [Hle | Hgt];
-    try destruct Hle as [Hlt | Heq].
-    + now constructor.
-    + assumption.
-    + revert n m H Hgt. induction lst' as [| k lst'' IHlst''].
-      * constructor; [assumption | constructor].
-      * intros. {
-        simpl. destruct (lt_eq_lt_dec n k) as [Hle | Hgt'];
-        try destruct Hle as [Hlt | Heq].
-        - constructor; [ assumption | constructor ]; try assumption.
-          now inversion_clear H.
-        - assumption.
-        - inversion_clear H.
-          constructor; try assumption. now apply IHlst''. }
-Defined.
+  Definition inject (n : name) (ns : NameSet) : NameSet.
+  (* the function Injects a name 'n' into a finite set of names 'ns' -------- *)
+  Proof.
+    destruct ns as (lst, H). pose (aux_inject n lst) as nlst.
+    exists nlst. subst nlst.
+    destruct lst as [| m lst'].
+    - constructor.
+    - simpl. destruct (lt_eq_lt_dec (M.id n) (M.id m)) as [Hle | Hgt];
+      try destruct Hle as [Hlt | Heq].
+      + now constructor.
+      + assumption.
+      + revert n m H Hgt. induction lst' as [| k lst'' IHlst''].
+        * constructor; [assumption | constructor].
+        * intros. {
+          simpl. destruct (lt_eq_lt_dec (M.id n) (M.id k)) as [Hle | Hgt'];
+          try destruct Hle as [Hlt | Heq].
+          - constructor; [ assumption | constructor ]; try assumption.
+            now inversion_clear H.
+          - assumption.
+          - inversion_clear H.
+            constructor; try assumption. now apply IHlst''. }
+  Defined.
+
+  Lemma post_inject : ∀ n ns, In n (inject n ns).
+  Proof.
+    intros. revert n.
+    destruct ns as [lst H]. simpl.
+    induction lst as [| m lst' IHlst']; intro.
+    - now constructor.
+    - simpl. destruct (lt_eq_lt_dec (M.id n) (M.id m)) as [Hle | Hgt];
+      try destruct Hle as [Hlt | Heq].
+      + now constructor.
+      + constructor. now apply M.id_inj.
+      + right. apply IHlst'. inversion_clear H; [constructor | assumption].
+  Qed.
+
+  Lemma inject_discr : ∀ n m ns, In m (inject n ns) → n = m \/ In m ns.
+  Proof.
+    intros. revert n m H.
+    destruct ns as [lst H]. simpl.
+    induction lst as [| k lst' IHlst']; intros.
+    - elim H0; intro; [now left | contradiction].
+    - destruct (eq_dec n m) as [H1 | H1].
+      + now left.
+      + right. destruct (eq_dec m k)  as [H2 | H2].
+        * now left.
+        * right. {
+          assert (increasing lst'). { 
+            inversion_clear H; [constructor | assumption]. }
+          pose (Hlst' := IHlst' H3). simpl in H0.
+          destruct (lt_eq_lt_dec (M.id n) (M.id k)) as [Hle | Hgt];
+          try destruct Hle as [Hlt | Heq].
+          - 
+
+  Definition declare (lst : list name) : NameSet.
+  Proof.
+    induction lst as [| n lst' ns'].
+    - exact nothing.
+    - exact (inject n ns').
+  Defined.
+
+  Lemma post_declare : ∀ lst n, In n (declare lst) ↔ In n lst.
+  Proof.
+    intro.
+    induction lst as [| m lst' IHlst']; intro; split; intro;
+    try contradiction.
+    - simpl in H |-*. destruct (eq_dec m n).
+      + now left.
+      + right. apply IHlst'. destruct lst' as [| k lst''].
+        * simpl in H. destruct H as [H1 | H1]; contradiction.
+        * apply IHlst'. { destruct (eq_dec n k) as [H1 | H1].
+          - now left.
+          - apply IHlst'. simpl.
+
+End Names.
+
+
+Inductive var := v : nat → var.
+Module Var <: NAME with Definition name := var.
+  Definition name := var.
+  Definition id := fun x : name => let '(v n) := x in n.
+
+  Lemma id_inj : ∀ x y : name, id x = id y → x = y.
+  Proof. intros. destruct x as [n], y as [m]. simpl in H. now rewrite H. Qed.
+
+  Lemma id_surj : ∀ n : nat, ∃ x : name, id x = n.
+  Proof. intro. now exists (v n). Qed.
+
+  Definition eq_dec : ∀ x y : name, {x = y} + {x ≠ y}.
+  Proof.
+    intros. destruct x as [n], y as [m].
+    destruct (Nat.eq_dec n m) as [HEq | HNeq].
+    - left. now rewrite HEq.
+    - right. intro. apply HNeq. now injection H.
+  Defined.
+End Var.
+
+Module varScopes := Names(Var).
+
+
 
 Fixpoint aux_segment (base len : nat) {struct len} : list nat :=
 (* the function generates the list [base; ..., base + len - 1].
